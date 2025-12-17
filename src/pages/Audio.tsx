44ea44fa44e3c414a987c7655d66314b0c7c6203
stanggren <astanggren@gmail.com>
2025-12-17 18:06:44 +0100

    audio

--- src/pages/Audio.tsx
+++ src/pages/Audio.tsx
@@ -1,55 +1,38 @@
 const Audio = () => {
-  const sectionOne = [
-    {
-      name: 'Project 1',
-      url: '#',
-      role: 'Role',
-      tech: 'Tech stack',
-    },
-  ];
-
-  const sectionTwo = [
-    {
-      name: 'Project 1',
-      url: '#',
-      tech: 'Tech stack',
-    },
-  ];
-
   return (
     <div>
       <h1 className="font-bold pb-24">Audio</h1>
-
       <section>
-        <h2 className="font-bold">Section One</h2>
-        <ul className="mt-16">
-          {sectionOne.map((work) => (
-            <li key={work.name} className="mb-16">
-              <h3 className="font-bold">
-                <a href={work.url} target="_blank" rel="noopener noreferrer" className="hover:text-blue-700">
-                  {work.name}
-                </a>
-              </h3>
-              <p>{work.role}</p>
-              <p className="text-sm">{work.tech}</p>
-            </li>
-          ))}
-        </ul>
-      </section>
+        <ul>
+          <li className="mb-16">
+            <h3 className="font-bold">
+              <a href="https://open.spotify.com/album/0YCGEGY6JE5T89NRkN6FFq?si=SSwUFyzBTxmJojL21FrP8Q" target="_blank" rel="noopener noreferrer" className="hover:text-blue-700">
+                Tåni Såprano EP
+              </a>
+            </h3>
+            <p>Music</p>
+          </li>
+          <li className="mb-16">
+            <a href="https://soundcloud.com/andreas-stanggren" target="_blank" rel="noopener noreferrer" className="font-bold hover:text-blue-700">
+              Individual work
+            </a>
+            <p>Music and sound art</p>
+          </li>
+          <li className="mb-16">
+            <h3 className="font-bold">
+              <a href="https://store.steampowered.com/app/2646620/White_Hook_River/" target="_blank" rel="noopener noreferrer" className="hover:text-blue-700">
+                White Hook River
+              </a>
+            </h3>
+            <p>Music & SFX</p>
+            <p className="text-sm">Winner of Umeå University Computer Science Game Jam 2024</p>
+            <p className="text-sm">
+              <a href="https://frostspektrum.itch.io/hookem" target="_blank" rel="noopener noreferrer" className="hover:text-blue-700">
+                Play demo on itch.io
+              </a>
+            </p>
+          </li>
 
-      <section className="mt-48">
-        <h2 className="font-bold">Section Two</h2>
-        <ul className="mt-16">
-          {sectionTwo.map((work) => (
-            <li key={work.name} className="mb-16">
-              <h3 className="font-bold">
-                <a href={work.url} target="_blank" rel="noopener noreferrer" className="hover:text-blue-700">
-                  {work.name}
-                </a>
-              </h3>
-              <p className="text-sm">{work.tech}</p>
-            </li>
-          ))}
         </ul>
       </section>
     </div>
