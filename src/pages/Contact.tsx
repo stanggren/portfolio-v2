d4c3c50936a7a4f3c9189a99b9f950fe4959e10d
stanggren <astanggren@gmail.com>
2025-12-18 15:56:00 +0100

    timing

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -7,7 +7,7 @@ const Contact = () => {
   return (
     <div>
       <h1 className="font-bold pb-24">
-        <ScrambleText text={t('contact.title')} scrambleDuration={400} scrambleSpeedStart={50} scrambleSpeedEnd={150} pixelBlur />
+        <ScrambleText text={t('contact.title')} scrambleDuration={300} scrambleSpeedStart={30} scrambleSpeedEnd={150} pixelBlur />
       </h1>
       <section>
         <ul>
--- src/pages/Web.tsx
+++ src/pages/Web.tsx
@@ -64,7 +64,7 @@ const Web = () => {
   return (
     <div>
       <h1 className="font-bold pb-24">
-        <ScrambleText text={t('web.title')} scrambleDuration={400} scrambleSpeedStart={50} scrambleSpeedEnd={150} pixelBlur />
+        <ScrambleText text={t('web.title')} scrambleDuration={300} scrambleSpeedStart={30} scrambleSpeedEnd={150} pixelBlur />
       </h1>
 
       <section>
